b6fca332f2842405b4d7b4cadec316adc403c082
johncban <johncban@gmail.com>
2019-04-22 06:52:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,12 +4,12 @@ def my_each(collection) # put argument(s) here
   # code here
   if block_given?
     i = 0
- 
+
     while i < collection.length
       yield(collection[i])
       i = i + 1
     end
- 
+
     collection
   else
     puts "Hey! No block was given!"
